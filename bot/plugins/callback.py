150039afcda7b67ee5ae624a4b8702dc27b3d4b0
Hitenop <87300913+Hitenop@users.noreply.github.com>
2021-08-16 15:31:57 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>Found</i> <code>{leng}</code> <i>Results For Your Query:</i> <code>{query}</code>"
+    text=f"🥳<i>Got</i> <code>{leng}</code> <i>Results For Your Request:</i> <code>{query}</code>"
         
     try:
         await update.message.edit(
